ef448973cf824ba9650e8d4abc978bbb113ceadb
5n0weirdo <snoweirdo@protonmail.com>
2021-10-10 06:57:41 +0200

    1-Template Literals

---
+++ 1-Template Literals/01/index.js
@@ -0,0 +1,18 @@
+let firstName = "Web";
+let lastName = "Developer ";
+let age = 19;
+let age2 = 27;
+
+// const fullName = ` ${age + age2} ${firstName} ${lastName}`;
+
+// console.log(fullName);
+
+let example = "Hello \n " + "World";
+
+window.onload = function() {
+        thing();
+        
+        function thing() {
+            document.getElementById("example").innerText = example;
+        };
+}
