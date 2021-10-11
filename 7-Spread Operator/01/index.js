5c74dc5e36636b05a5d9c56babd7e0ce743a54b3
5n0weirdo <snoweirdo@protonmail.com>
2021-10-11 07:08:19 +0200

    Spread Operator

---
+++ 7-Spread Operator/01/index.js
@@ -0,0 +1,15 @@
+let example1 = [2, 7, 9, 20];
+let example2 = [...example1];
+
+example2.push(3);
+
+console.log(example2);
+
+let person = {
+    firstName: "Devs"
+};
+
+// let person2 = {
+//     ...person
+// }
+//person2 = {firstName: "Devs"};
